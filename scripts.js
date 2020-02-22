076f00bb17e3b622d13d4b99da014bec7f8e11a7
Pedro Oliveira <56042296+Pedrooliver13@users.noreply.github.com>
2020-02-22 12:41:53 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('input[name="price"]')
+input.addEventListener('keydown' , (e)=>{
+    setTimeout(()=>{
+      let {value} = e.target
+
+      value = value.replace(/\D/g ,"")
+
+      e.target.value = value
+    } , 1)
+})
